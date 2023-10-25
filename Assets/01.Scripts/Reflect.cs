6254a53cd91b71a3cd98d132d8b2f9f6fbc1dbbe
DAZB <jiwoo2426p@gmail.com>
2023-10-26 01:51:18 +0900

    반사각

---
+++ Assets/01.Scripts/Reflect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflect : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private Vector3 Velocity;
+
+    private void Start() {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.AddForce(Random.insideUnitCircle.normalized * 5, ForceMode2D.Impulse);
+    }
+
+    private void Update() {
+        Velocity = rigid.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        var speed = Velocity.magnitude;
+        var dir = Reflection(Velocity.normalized, other.contacts[0].normal);
+        rigid.velocity = dir * Mathf.Max(speed, 0);
+    }
+
+    public Vector3 Reflection(Vector3 V, Vector3 N) {
+        return V - 2 * Vector3.Dot(V, N) * N;
+    }
+}
